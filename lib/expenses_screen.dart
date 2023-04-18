0a7b87590f622852f64e9ef3957b3fb893bb848b
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-04-18 15:41:26 +0530

    Added Snackbar for Undo

--- lib/expenses_screen.dart
+++ lib/expenses_screen.dart
@@ -45,9 +45,27 @@ class _ExpensesScreenState  extends State<ExpensesScreen> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+
     setState(() {
       _registeredExpenses.remove(expense);
     });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration:const  Duration(seconds: 3),
+        content: const Text('Expense Deleted'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      )
+    );
   }
 
   @override
